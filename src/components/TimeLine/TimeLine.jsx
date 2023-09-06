e5a312c07876af0f2d03b157eb4e2d59c0e391f4
Diana Sen <diana_sen@epam.com>
2023-09-06 00:21:38 -0600

    feat: add data fetching, loading and error functionality for timeline with built-in react hooks

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate } from '@fortawesome/free-solid-svg-icons';
 import Info from '../Info/Info';
 
-const TimeLine = ({ data }) => {
-	return (
-		<div className='timeline'>
-			<ul className='timeline-list'>
-				{data.map((item) => (
-					<li className='timeline-item-container' key={item.title}>
-						<div className='timeline-date'>{item.date}</div>
-						<Info text={item.text}>
-							<h3>{item.title}</h3>
-						</Info>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+const TimeLine = ({ data, isLoading, hasError }) => {
+	if (!isLoading && !hasError) {
+		return (
+			<div className='timeline'>
+				<ul className='timeline-list'>
+					{data.map((item) => (
+						<li className='timeline-item-container' key={item.title}>
+							<div className='timeline-date'>{item.date}</div>
+							<Info text={item.text}>
+								<h3>{item.title}</h3>
+							</Info>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	} else if (isLoading) {
+		return (
+			<div className='timeline-load'>
+				<FontAwesomeIcon className='timeline-icon' icon={faRotate} />
+			</div>
+		);
+	} else {
+		return (
+			<div className='timeline-error'>
+				Something went wrong: please review your server connection!
+			</div>
+		);
+	}
 };
 
 export default TimeLine;
--- src/pages/Inner/Inner.jsx
+++ src/pages/Inner/Inner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Panel from '../../components/Panel/Panel';
 import Box from '../../components/Box/Box';
 import Button from '../../components/Button/Button';
@@ -11,16 +11,41 @@ import {
 	ABOUT_DATA,
 	EXPERIENCE_DATA,
 	FEEDBACK_DATA,
-	TIMELINE_DATA,
+	//TIMELINE_DATA,
 } from '../../utils/constants';
 import Portfolio from '../../components/Portfolio/Portfolio';
 import Feedback from '../../components/Feedback/Feedback';
 const Inner = () => {
 	const [panelVisible, setPanelVisible] = useState(true);
+	const [educationData, setEducationData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [hasError, setHasError] = useState(false);
 
 	const togglePanel = () => {
 		setPanelVisible(!panelVisible);
 	};
+
+	const fetchEducationData = () => {
+		setIsLoading(true);
+		fetch('/api/educations')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setIsLoading(false);
+				setEducationData(data);
+			})
+			.catch((e) => {
+				console.log(e);
+				setIsLoading(false);
+				setHasError(true);
+			});
+	};
+
+	useEffect(() => {
+		fetchEducationData();
+		console.log(educationData);
+	}, []);
 
 	return (
 		<main>
@@ -43,7 +68,13 @@ const Inner = () => {
 					></Box>
 					<Box
 						title={'Education'}
-						content={<TimeLine data={TIMELINE_DATA} />}
+						content={
+							<TimeLine
+								data={educationData}
+								isLoading={isLoading}
+								hasError={hasError}
+							/>
+						}
 						reference={'education'}
 					></Box>
 					<Box
