96f3d0fd34bb098a245a54d304197ecc26510afa
Diana Sen <diana_sen@epam.com>
2023-09-06 00:25:57 -0600

    feat: create initial store with redux-toolkit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import server from './services/server';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import './assets/styles/main_style.scss';
 
@@ -11,7 +13,9 @@ server();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
 
